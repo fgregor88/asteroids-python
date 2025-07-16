e1c7b2dc50657ba0f15af26603d83e287ac58218
Filip Gregor <filip.gregor@gmail.com>
2025-07-16 20:01:36 +0200

    player drawn and added .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -13,11 +14,15 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
