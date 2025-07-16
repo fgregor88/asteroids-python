b94e48118203730c40e37cfef1b41a41c2c3a5ed
Filip Gregor <filip.gregor@gmail.com>
2025-07-16 19:11:40 +0200

    fps

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
 
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -16,6 +19,8 @@ def main():
                 return
         screen.fill(color="black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
